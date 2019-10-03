06a1ec1a109f5f5d15400a0c517e2f5cec7d2c7f
Florencia Malenchini <florenciamalenchini@gmail.com>
2019-10-03 12:41:20 -0300

    Criação do game

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by sacdigital on 23/09/19.
+//  Copyright © 2019 sacdigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var card: UIButton!
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
+
